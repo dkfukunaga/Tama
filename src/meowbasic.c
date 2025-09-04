bc26f3fddb658e9bfbd6229d5fcb138dd3a22846
dkfukunaga <dkfukunaga@gmail.com>
2025-09-04 12:34:41 -0700

    some additional testing to see how it works

--- src/meowbasic.c
+++ src/meowbasic.c
@@ -11,9 +11,15 @@ int main(int argc, const char* argv[]) {
     int con2 = addConstant(&chunk, 3.14);
     writeChunk(&chunk, OP_CONSTANT, 123);
     writeChunk(&chunk, con2, 123);
+    int con3 = addConstant(&chunk, 2.71);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, con3, 123);
+    int con4 = addConstant(&chunk, 123.456);
+    writeChunk(&chunk, OP_CONSTANT, 124);
+    writeChunk(&chunk, con4, 124);
 
 
-    writeChunk(&chunk, OP_RETURN, 125);
+    writeChunk(&chunk, OP_RETURN, 250);
 
     disassembleChunk(&chunk, "test chunk");
     freeChunk(&chunk);
